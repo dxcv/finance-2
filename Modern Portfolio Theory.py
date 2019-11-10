d14ee8a3f516148fb47a87fe6b454ec6b9c61806
coffeetimegit <57280286+coffeetimegit@users.noreply.github.com>
2019-11-10 14:53:51 +0900

    Update Modern Portfolio Theory.py

--- Modern Portfolio Theory.py
+++ Modern Portfolio Theory.py
@@ -5,15 +5,17 @@ import pandas as pd
 from pylab import plt
 
 
-raw = pd.read_csv('/Users/username/Documents/finance/price_table.csv', index_col=0, parse_dates=True).dropna()
-symbols = pd.read_csv('/Users/username/Documents/finance/price_table.csv', nrows=0)
+price_table = '/Users/yutakaobi/Documents/finance/price_table.csv'
+
+raw = pd.read_csv(price_table, index_col=0, parse_dates=True).dropna()
+symbols = pd.read_csv(price_table, nrows=0)
 
 
 products = []
 for symbol in symbols:
   if symbol.lower() != 'date':
       products.append(symbol)
-print('Investment Universe:\n', products, '\n')
+print('Investment Universe: ' + str(products), '\n')
 
 
 noa = len(products)
@@ -50,11 +52,11 @@ eweights = np.array(noa * [1 / noa])
 
 
 opts = sco.minimize(Minimum_Sharpe_Ratio, eweights, method='SLSQP', bounds=bnds, constraints=cons)
-print('Minimum Sharpe Ratio')
+print('Maximum Sharpe Ratio details:')
 print('Sharpe Ratio: ' + str(Return(opts['x']) / Volatility(opts['x'])))
-print('Return with minimum sharpe ratio: ' + str(Return((opts['x'])).round(3)))
-print('Volatility with minimum sharpe ratio: ' + str(Volatility(opts['x']).round(3)))
-print('Allocation details')
+print('Return with maximun sharpe ratio: ' + str(Return((opts['x'])).round(3)))
+print('Volatility with maximum sharpe ratio: ' + str(Volatility(opts['x']).round(3)))
+print('Allocation details to achieve maximum sharpe ratio:')
 for allocation in range(len(opts['x'].round(3))):
    print('Allocation for ' + str(products[allocation]) + ': ' +
          str(opts['x'].round(3)[allocation]))
@@ -62,11 +64,11 @@ print('\n')
 
 
 optv = sco.minimize(Volatility, eweights, method='SLSQP', bounds=bnds, constraints=cons)
-print('Minimum Volatility')
+print('Minimum Volatility details:')
 print('Volatility: ' + str(Volatility(optv['x']).round(3)))
 print('Return with minimum volatility: ' + str(Return(optv['x']).round(3)))
 print('Sharpe Ratio: ' + str(Return(optv['x']) / Volatility(optv['x'])))
-print('Allocation details')
+print('Allocation details to achieve minimum volatility:')
 for allocation in range(len(optv['x'].round(3))):
    print('Allocation for ' + str(products[allocation]) + ': ' +
          str(optv['x'].round(3)[allocation]))
@@ -76,7 +78,8 @@ cons = ({'type': 'eq', 'fun': lambda x: Return(x) - tret},
       {'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
 bnds = tuple((0, 1) for x in weights)
 
-trets = np.linspace(Return(optv['x']), Return(opts['x']), 50)
+
+trets = np.linspace(Return(optv['x']),Return(opts['x']), 50)
 tvols = []
 for tret in trets:
   res = sco.minimize(Volatility, eweights, method='SLSQP',
@@ -88,10 +91,10 @@ tvols = np.array(tvols)
 plt.figure(figsize=(10, 6))
 plt.scatter(pvols, prets, c=prets / pvols, marker='.', alpha=0.8, cmap='coolwarm')
 plt.plot(tvols, trets, 'b', lw=2.5)
-plt.plot(Volatility(opts['x']), Return(opts['x']),
-       'y*', markersize=15)
 plt.plot(Volatility(optv['x']), Return(optv['x']),
-       'r*', markersize=15)
+       'r*', markersize=10)
+plt.plot(Volatility(opts['x']), Return(opts['x']),
+       'y*', markersize=10)
 plt.xlabel('expected volatility')
 plt.ylabel('expected return')
 plt.colorbar(label='Sharpe ratio')

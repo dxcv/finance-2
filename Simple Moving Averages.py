e9fcc76ddd2c976263c2bec7b99924f848974fc0
coffeetimegit <57280286+coffeetimegit@users.noreply.github.com>
2019-11-10 16:40:25 +0900

    Update Simple Moving Averages.py

--- Simple Moving Averages.py
+++ Simple Moving Averages.py
@@ -1,21 +1,27 @@
 import numpy as np
 import pandas as pd
-from pylab import plt, mpl
+from pylab import plt
 
 
 price_table = '/Users/username/Documents/finance/GOOGLE-Table 1.csv' #full folder path
 data = pd.read_csv(price_table, index_col=0)
+rows = pd.read_csv(price_table, nrows=0, index_col=0)
 
-SMA1 = 42
-SMA2 = 252
+product = []
+for r in rows:
+  if r.lower() != 'date':
+      product.append(r)
 
-data['SMA1'] = data['GOOGLE'].rolling(SMA1).mean()
-data['SMA2'] = data['GOOGLE'].rolling(SMA2).mean()
+SMAs1 = 42
+SMAs2 = 252
+
+data['SMAs1'] = data[product].rolling(SMAs1).mean()
+data['SMAs2'] = data[product].rolling(SMAs2).mean()
 
 data.plot(figsize=(10, 6))
 
 data.dropna(inplace=True)
-data['Position'] = np.where(data['SMA1'] > data['SMA2'], 1, -1)
+data['Position'] = np.where(data['SMAs1'] > data['SMAs2'], 1, -1)
 print(data)
 
 ax = data.plot(secondary_y='Position', figsize=(10, 6))
